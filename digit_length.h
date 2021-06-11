d372938d29bdc21e51351fb4a89c0e76676aeb89
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2021-06-12 02:13:03 +0300

    Update digit_length.h

--- digit_length.h
+++ digit_length.h
@@ -69,29 +69,4 @@ std::vector<int8_t> DigitLengths(std::vector<int32_t> digits){
     return lens;
 }
 
-//int main(int argc, const char * argv[]) {
-//    
-//    std::vector<int32_t> numbers={1,2,3,54,3,5667,23,1,2,4,56, 39484, 492837, 2,3,45,7263, 476, 2,1,20 ,0, 0, 230, 94, 2930, 3948, 4,5, 6, 3,40,6};
-//    
-//    std::vector<int8_t> length = DigitLengths(numbers);
-//    
-//    auto dict = NumToString(numbers);
-//    
-//    auto compressed = CompactVec(dict);
-//
-//    std::cout<< "size1:" << numbers.size() << std::endl;
-//    std::cout<< "size2:" << compressed.size() << std::endl;
-//    
-//    // reconstruct numbers
-//    // backt o string
-//    std::cout << "back to strings:" << std::endl;
-//    auto dict2 = NumToString(compressed);
-//    
-//    auto numbers2 = StringToNums(&dict2, &length);
-//    for(auto a: numbers2){
-//        std::cout << a << std::endl;
-//    }
-//    
-//
-//    return 0;
-//}
+
